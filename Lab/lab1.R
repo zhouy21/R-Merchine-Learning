7efe4089e77e2b850fe863580792b31eb9bd5ff6
Yunlei Zhou <zhouy21@outlook.com>
2019-09-27 11:00:27 -0400

    Finished the Lab1

--- Lab/lab1.R
+++ Lab/lab1.R
@@ -13,8 +13,9 @@ theme_set(theme_bw())
 dd <- read_tsv("Lab/dataset/cogo-train.tsv")
 str(dd)
 
-### Describing the data
-## 1. What fraction of users use each browser?
+
+### Describing the data ==================================================
+## 1. What fraction of users use each browser? 
 cogo_browsers <- dd %>% 
   summarise(
     p_chrome = mean(browser1), 
@@ -56,7 +57,7 @@ ggplot(cogo_devices, aes(device, p)) +
   scale_x_discrete("Device") + 
   scale_y_continuous("% Cogo Users", label=percent_format())
 
-## 3. What fraction of users use more than one browser?
+## 3. What fraction of users use more than one browser? 
 # create new variables: 
 dd %<>% 
   mutate(num_browsers = browser1 + browser2 + browser3) %>% 
@@ -65,7 +66,7 @@ dd %<>%
 dd %>%
   summarise(mean(two_or_more_browsers))
 
-## 4. What is the distribution of email open-rates?
+## 4. What is the distribution of email open-rates? 
 # plot a histogram
 ggplot(dd, aes(p_open)) + 
   geom_histogram()
@@ -77,8 +78,7 @@ ggplot(dd, aes(p_open)) +
   stat_ecdf()
 
 
-
-### Regression:
+### Regression: ==========================================================
 ## 1. Creating irrelevant predictors: ir
 dd %>% mutate(
   ir0=runif(n(),min=0,max=10000),
@@ -105,7 +105,7 @@ dd.test <- dd[test_index,]
 dd.train <- dd[-test_index,]
 
 
-## 3. Linear regression
+## 3. Linear regression:==================================================
 str(dd)
 f1 <- as.formula(p_open ~ browser1 + browser2 + browser3) 
 f2 <- as.formula(p_open ~ num_browsers + age + gender)  
@@ -114,23 +114,64 @@ f2 <- as.formula(p_open ~ num_browsers + age + gender)
 y.train <- dd.train$p_open 
 y.test <- dd.test$p_open
 
-# Create Linear regression based on "dd.train"
+# 1.Create Linear regression based on "dd.train"
 fit.lm1 <- lm(f1, dd.train)
 
-# Predict open rate
+# 2.Predict open rate
 yhat.train.lm1 <- predict(fit.lm1, dd.train) 
 
-# Calculate MSE
+# 3.Calculate MSE
 mse.train.lm1 <- mean((y.train - yhat.train.lm1)^2)
 
-# Caluculate Test data 
+# 4.Test 
 yhat.test.lm1 <- predict(fit.lm1, dd.test) 
 mse.test.lm1 <- mean((y.test - yhat.test.lm1)^2)
 
+# coefficent: coef()
+coef(fit.lm1)
 
-# Training using a small training sample
+
+## use small training sample: =============================================
+dd.train.small.sample.size <- 30 
+dd.train.small.sample <- dd.train[sample(nrow(dd.train), dd.train.small.sample.size),]
+y.train.small.sample <- dd.train.small.sample$p_open
+
+fit.lm1.small <- lm(f1, dd.train.small.sample)
+yhat.train.lm1.small <- predict(fit.lm1.small, dd.train.small.sample) 
+mse.train.lm1.small <- mean((y.train.small.sample - yhat.train.lm1.small)^2)
+mse.train.lm1.small # the MSE is smaller
 
 
+## Importance of randomness: =============================================
+# collect data with age>26
+dd.train.nr <- dd.train[dd.train$age>26,] 
+y.train.nr <- dd.train.nr$p_open
+
+# create a random sample of the same size for comparison:
+dd.train.r <- dd.train[sample(nrow(dd.train), nrow(dd.train.nr)),] 
+y.train.r <- dd.train.r$p_open
+
+# non-random data 
+fit.lm2.nr <- lm(f2, dd.train.nr)
+
+yhat.train.lm2.nr <- predict(fit.lm2.nr, dd.train.nr)  
+mse.train.lm2.nr <- mean((y.train.nr - yhat.train.lm2.nr)^2)
+
+yhat.test.lm2.nr <- predict(fit.lm2.nr, dd.test)   
+mse.test.lm2.nr <- mean((y.test - yhat.test.lm2.nr)^2)
+
+# random data
+fit.lm2.r <- lm(f2, dd.train.r)
+
+yhat.train.lm2.r <- predict(fit.lm2.r, dd.train.r)  
+mse.train.lm2.r <- mean((y.train.r - yhat.train.lm2.r)^2)
+
+yhat.test.lm2.r <- predict(fit.lm2.r, dd.test)  
+mse.test.lm2.r <- mean((y.test - yhat.test.lm2.r)^2)
+
+# Compare
+mse.test.lm2.nr
+mse.test.lm2.r
 
 
 
